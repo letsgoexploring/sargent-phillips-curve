bc2bf30ef21b5d8ae5ddcea86353fa66dd74317c
Brian Jenkins <letsgoexploring@gmail.com>
2016-03-07 19:32:06 -0800

    updated

--- sargentPhillipsCurveAnimation.py
+++ sargentPhillipsCurveAnimation.py
@@ -79,14 +79,14 @@ def update_plot(i):
 
 # Set up formatting for the movie files
 Writer = animation.writers['ffmpeg']
-writer = Writer(fps=10, metadata=dict(artist='Brian C Jenkins'), bitrate=5000)
+writer = Writer(fps=10, metadata=dict(artist='Brian C Jenkins'), bitrate=7500)
 
 # Make the animation
 ani = animation.FuncAnimation(fig, update_plot, frames = n,fargs = (), blit=False,repeat=False,interval=20)
 
 # Save the animation as .mp4
-ani.save('US_Inflation_Unemployment_Monthly_BP_Filtered.ogv',fps=10,codec='libtheora')
-# ani.save('US_Inflation_Unemployment_Monthly_BP_Filtered.mp4',writer=writer)
+# ani.save('US_Inflation_Unemployment_Monthly_BP_Filtered.ogv',fps=10,codec='libtheora')
+ani.save('US_Inflation_Unemployment_Monthly_BP_Filtered.mp4',writer=writer)
 
 # Save the final image of the animation to use as the still image placeholder
 plt.savefig('US_Inflation_Unemployment_Monthly_BP_Filtered.png',bbox_inches='tight',dpi=120)
@@ -96,6 +96,5 @@ plt.savefig('US_Inflation_Unemployment_Monthly_BP_Filtered.png',bbox_inches='tig
 # In[6]:
 
 # Convert the mp4 video to ogg format
-# makeOgg = 'ffmpeg -i US_Inflation_Unemployment_Monthly_BP_Filtered.mp4 -acodec libvorbis -ac 2 -ab 128k -ar 44100 -b:v 1800k  US_Inflation_Unemployment_Monthly_BP_Filtered.ogv'
-# subprocess.call(makeOgg,shell=True)
-
+makeOgg = 'ffmpeg -i US_Inflation_Unemployment_Monthly_BP_Filtered.mp4 -acodec libvorbis -ac 2 -ab 128k -ar 44100 -b:v 1800k  US_Inflation_Unemployment_Monthly_BP_Filtered.ogv'
+subprocess.call(makeOgg,shell=True)
