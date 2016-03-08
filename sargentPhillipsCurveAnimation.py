70de16f1e5f593634ce0da7e475cfe0e80341869
Brian Jenkins <letsgoexploring@gmail.com>
2016-03-07 18:42:21 -0800

    update

--- sargentPhillipsCurveAnimation.py
+++ sargentPhillipsCurveAnimation.py
@@ -85,7 +85,7 @@ writer = Writer(fps=10, metadata=dict(artist='Brian C Jenkins'), bitrate=5000)
 ani = animation.FuncAnimation(fig, update_plot, frames = n,fargs = (), blit=False,repeat=False,interval=20)
 
 # Save the animation as .mp4
-ani.save('US_Inflation_Unemployment_Monthly_BP_Filtered.ogv',fps=10,codec=libx264)
+ani.save('US_Inflation_Unemployment_Monthly_BP_Filtered.ogv',fps=10,codec='libtheora')
 # ani.save('US_Inflation_Unemployment_Monthly_BP_Filtered.mp4',writer=writer)
 
 # Save the final image of the animation to use as the still image placeholder
